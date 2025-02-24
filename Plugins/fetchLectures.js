5c8966323ceba09831ab07f94297bd7d082b84d4
hello world <159936375+tk22kalal2@users.noreply.github.com>
2025-02-24 23:33:45 +0530

    Update fetchLectures.js

--- Plugins/fetchLectures.js
+++ Plugins/fetchLectures.js
@@ -1,6 +1,6 @@
 async function loadLectures(tableId, startId, endId) {
     try {
-        const response = await fetch('../Plugins/lectures.json?nocache=' + new Date().getTime());
+        const response = await fetch('lectures.json?nocache=' + new Date().getTime());
         const lectures = await response.json();
 
         const tableBody = document.querySelector(`#${tableId} tbody`);
